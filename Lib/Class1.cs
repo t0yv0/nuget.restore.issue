0cb2f663e90cba2d3341822af2de69952f0d2cc4
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-06-10 14:48:25 -0400

    Sample code.

---
+++ Lib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml;
+
+namespace Lib
+{
+    public class Class1
+    {
+        public static void Foo()
+        {
+            XmlAttribute a = null;
+            a.Clone();
+        }
+    }
+}
